fc1a60486541aae69c89e6eba2a9ca48d0468926
Polyreef <a.down636@passinbox.com>
2025-11-18 13:21:50 -0500

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def main():
+    if len(sys.argv) == 1:
+        print(f"Használat: python {sys.argv[0]} [--ui | <test_dir_path>]")
+        sys.exit(1)
+
+    if sys.argv[1] == "--ui":
+        pass
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    main()
